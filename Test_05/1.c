e09c9122a00c84617548c2d7fd22c316f29c8685
cheeli_anil <cheelianil14@gmail.com>
2024-08-11 16:08:06 +0530

    Test_05

---
+++ Test_05/1.c
@@ -0,0 +1,32 @@
+ - Test
+Date: 13-03-2023
+
+    1. Give a fastest way to multiply any number by 9.
+
+    2. How to measure the size of any variable without "sizeof" operator?
+    
+    3. Write code snippets to swap two variables in five different ways.
+    
+    4. What is the efficient way to divide a no. by 4?
+    
+    5. Suggest an efficient method to count the no. of l's in a 32 bit no. Remember without using loop &testing each bit
+    
+    6. Test whether a no. is power of 2 or not.
+    
+    7. How to check endianness of the computer.
+    
+    8. Write a C-program which does the addition of two integers without using '+' operator.
+    
+    9. Write a C-program to find the smallest of three integers without using any of the comparision operators.
+    
+    10. Find the maximum & minimum of two numbers in a single line without using any condition & loop.
+    
+    11. How to print number from 1 to 100 without using conditional operators.
+    
+    12. WAP to print 100 times "Hello" without using loop & goto statement.
+    
+    13. Write a program to print Full Pyramid Pattern.
+    
+    14. C program to find sum of integers in String
+    
+    15. C Program to perform right rotation by 2 in array
